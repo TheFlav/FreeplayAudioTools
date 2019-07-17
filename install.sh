b360e4ea93ddb1b1f4c684b5c15f2e93244feb67
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-07-17 11:17:08 -0600

    Updated for new RetroPie menu folder

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 INSTALL_DIR=/home/pi/Freeplay/$( ls /home/pi/Freeplay | grep -i freeplayaudiotools )
 
+mkdir -p "/home/pi/RetroPie/retropiemenu/Freeplay Options"
 cp $INSTALL_DIR/FreeplayAudioTools.sh "/home/pi/RetroPie/retropiemenu/Freeplay Options/FreeplayAudioTools.sh"
 
 if grep -q "Freeplay Audio Tools" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-	echo "Display Driver Menu Item Already Added"
+	echo "Audio Tools Menu Item Already Added"
 else
 	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay Options/FreeplayAudioTools.sh</path>\n\t\t<name>Freeplay Audio Tools</name>\n\t\t<desc>Tools to toggle between mono/stereo audio and enable HDMI audio</desc>\n\t\t<image>./icons/audiosettings.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
+
+exit 0
