36c5bb414cba6054e009dfbdf4ccbebb6b278f3e
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-08-20 13:08:24 -0600

    Removed reboots and revised messages

--- FreeplayAudioTools.sh
+++ FreeplayAudioTools.sh
@@ -5,13 +5,11 @@ INSTALL_DIR=/home/pi/Freeplay/$( ls /home/pi/Freeplay | grep -i freeplayaudiotoo
 audio_mono ()
 {
 	sudo cp $INSTALL_DIR/asound.conf /etc/asound.conf
-	sudo reboot
 }
 
 audio_stereo ()
 {
 	sudo rm /etc/asound.conf
-	sudo reboot
 }
 
 hdmi_on ()
@@ -49,7 +47,7 @@ for CHOICE in $CHOICES; do
 			dialog --title "Mono Audio" --yesno "This will mix the left and right audio channels so both can be heard on the speaker, but will also make the headphone output mono. Would you like to enable mono mixing?" 15 60
 			RESP=$?
 			case $RESP in
-				0) dialog --title "Mono Audio" --infobox "Enabling Mono Audio. System will shut down and settings will be applied on next boot." 10 60; audio_mono;;
+				0) dialog --title "Mono Audio" --infobox "Enabling Mono Audio. Settings will be applied on next boot." 10 60; audio_mono;;
 				1) dialog --title "Mono Audio" --infobox "NOT Enabling Mono Audio" 10 60;;
 				255) dialog --title "Mono Audio" --infobox "Returning to main menu" 10 60;;
 			esac
@@ -58,7 +56,7 @@ for CHOICE in $CHOICES; do
 			dialog --title "Stereo Audio" --yesno "This will separate the left and right audio channels, so the speaker will only play the right channel and headphones will have true stereo. Would you like to enable stereo audio?" 15 60
 			RESP=$?
 			case $RESP in
-				0) dialog --title "Stereo Audio" --infobox "Turning Stereo Audio On. System will shut down and settings will be applied on next boot." 10 60;audio_stereo;;
+				0) dialog --title "Stereo Audio" --infobox "Turning Stereo Audio On. Settings will be applied on next boot." 10 60;audio_stereo;;
 				1) dialog --title "Stereo Audio" --infobox "NOT Turning Stereo Audio" 10 60;;
 				255) dialog --title "Stereo Audio" --infobox "Returning to main menu" 10 60;;
 			esac
@@ -67,7 +65,7 @@ for CHOICE in $CHOICES; do
 			dialog --title "HDMI Audio On" --yesno "This will turn HDMI audio on, which may break compatibility with some displays. Would you like to enable HDMI audio?" 15 60
 			RESP=$?
 			case $RESP in
-				0) dialog --title "HDMI Audio On" --infobox "Turning HDMI Audio On. System will shut down and settings will be applied on next boot." 10 60;hdmi_on;;
+				0) dialog --title "HDMI Audio On" --infobox "Turning HDMI Audio On. Settings will be applied on next boot." 10 60;hdmi_on;;
 				1) dialog --title "HDMI Audio On" --infobox "NOT Turning HDMI Audio On" 10 60;;
 				255) dialog --title "HDMI Audio On" --infobox "Returning to main menu" 10 60;;
 			esac
@@ -76,7 +74,7 @@ for CHOICE in $CHOICES; do
 			dialog --title "HDMI Audio Off" --yesno "This will turn HDMI audio off, which has better comaptibility with most displays. Would you like to disable HDMI audio?" 15 60
 			RESP=$?
 			case $RESP in
-				0) dialog --title "HDMI Audio Off" --infobox "Turning HDMI Audio Off. System will shut down and settings will be applied on next boot." 10 60;hdmi_off;;
+				0) dialog --title "HDMI Audio Off" --infobox "Turning HDMI Audio Off. Settings will be applied on next boot." 10 60;hdmi_off;;
 				1) dialog --title "HDMI Audio Off" --infobox "NOT Turning HDMI Audio Off" 10 60;;
 				255) dialog --title "HDMI Audio Off" --infobox "Returning to main menu" 10 60;;
 			esac
